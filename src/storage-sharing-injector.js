9795adb812541c69ce18e1f69869d9cb6105e11e
orbitlens <orbit.lens@gmail.com>
2021-02-11 21:37:14 +0200

    Storage sharing injector fixes

--- src/storage-sharing-injector.js
+++ src/storage-sharing-injector.js
@@ -1,64 +1,66 @@
 if (window.parent !== window) { //we are inside an iframe
-    //message dispatcher func
-    function dispatch(method, req) {
-        return new Promise((resolve) => {
-            const reqKey = new Date().getTime() + Math.floor(Math.random() * 17179869180).toString(36)
+    if (window.innerHeight > 50) { //execute only in the dialog iframe, ignoring hidden implicit intents iframe transport
+        //message dispatcher func
+        function dispatch(method, req) {
+            return new Promise((resolve) => {
+                const reqKey = new Date().getTime() + Math.floor(Math.random() * 17179869180).toString(36)
 
-            function listener(event) {
-                //math requests by a unique request key to deal with concurrent requests
-                if (event.data.reqKey === reqKey) {
-                    resolve(event.data.response)
-                    //unbind the listener once we received the response
-                    window.removeEventListener('message', listener, false)
+                function listener(event) {
+                    //math requests by a unique request key to deal with concurrent requests
+                    if (event.data.reqKey === reqKey) {
+                        resolve(event.data.response)
+                        //unbind the listener once we received the response
+                        window.removeEventListener('message', listener, false)
+                    }
                 }
+
+                //listen for the parent window responses
+                window.addEventListener('message', listener, false)
+                //pass the request to the parent frame
+                window.parent.postMessage({
+                    type: 'shared-storage',
+                    method,
+                    reqKey,
+                    req
+                }, '*')
+            })
+        }
+
+        window.albedoStorageProvider = {
+            /**
+             * Load an item from the storage by its key
+             * @param {String} key
+             * @return {Promise<String>}
+             */
+            getItem(key) {
+                return dispatch('getItem', {key})
+                    .then(response => response.value)
+            },
+            /**
+             * Save and item to the storage
+             * @param {String} key
+             * @param {String} value
+             * @return {Promise<void>}
+             */
+            setItem(key, value) {
+                return dispatch('setItem', {key, value})
+            },
+            /**
+             * Remove an item from the storage by its key
+             * @param {String} key
+             * @return {Promise<void>}
+             */
+            removeItem(key) {
+                return dispatch('removeItem', {key})
+            },
+            /**
+             * List all storage keys
+             * @return {Promise<Array<String>>}
+             */
+            enumerateKeys() {
+                return dispatch('enumerateKeys', {})
+                    .then(response => response.keys)
             }
-
-            //listen for the parent window responses
-            window.addEventListener('message', listener, false)
-            //pass the request to the parent frame
-            window.parent.postMessage({
-                type: 'shared-storage',
-                method,
-                reqKey,
-                req
-            }, '*')
-        })
-    }
-
-    window.albedoStorageProvider = {
-        /**
-         * Load an item from the storage by its key
-         * @param {String} key
-         * @return {Promise<String>}
-         */
-        getItem(key) {
-            return dispatch('getItem', {key})
-                .then(response => response.value)
-        },
-        /**
-         * Save and item to the storage
-         * @param {String} key
-         * @param {String} value
-         * @return {Promise<void>}
-         */
-        setItem(key, value) {
-            return dispatch('setItem', {key, value})
-        },
-        /**
-         * Remove an item from the storage by its key
-         * @param {String} key
-         * @return {Promise<void>}
-         */
-        removeItem(key) {
-            return dispatch('removeItem', {key})
-        },
-        /**
-         * List all storage keys
-         * @return {Promise<Array<String>>}
-         */
-        enumerateKeys() {
-            return dispatch('enumerateKeys', {key})
-                .then(response => response.keys)
         }
     }
 } else {
@@ -75,7 +77,7 @@ if (window.parent !== window) { //we are inside an iframe
         const {req} = data
         switch (data.method) {
             case 'getItem': {
-                const {value} = await Storage.get({key: req.key})
+                const value = await Storage.get({key: req.key})
                 return dispatch({value})
             }
 
